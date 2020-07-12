eb62d791407cd21bac5f09a94df033423655ad30
Subhajit Saha <s.subhajit89@gmail.com>
2020-07-12 14:31:49 +0530

    Select card option

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, HostListener, ElementRef } from '@angular/core';
 import { CardType, CardIndex } from '../../types';
 
 @Component({
@@ -8,14 +8,30 @@ import { CardType, CardIndex } from '../../types';
 })
 export class CardComponent implements OnInit, OnChanges {
 
-  @Input() 
-  get index(): CardIndex{
+  @Input()
+  get index(): CardIndex {
     return this._index;
   }
   set index(value: CardIndex) {
     this._index = CardIndex[value];
   }
   @Input() type: CardType;
+  @Input() display: 'show' | 'hide' = 'show';
+
+  @HostListener('document:click', ['$event'])
+  outsideClick(event) {
+    if(this.cardRef.nativeElement.contains(event.target)) {
+      if (this.display === 'hide') {
+        event.preventDefault();
+        return;
+      }
+      this.selected = !this.selected;
+    } else {
+      this.selected = false;
+    }
+  }
+
+  selected: boolean = false;
 
   private _index: CardIndex;
 
@@ -23,7 +39,7 @@ export class CardComponent implements OnInit, OnChanges {
 
   private readonly iconSet: Array<{ color: '#FF0000' | '#000', iconUnicode: '♣' | '♦' | '♥' | '♠' }> = [];
 
-  constructor() {
+  constructor(private cardRef: ElementRef) {
     this.iconSet.push({ iconUnicode: '♠', color: '#000' });
     this.iconSet.push({ iconUnicode: '♥', color: '#FF0000' });
     this.iconSet.push({ iconUnicode: '♦', color: '#FF0000' });
